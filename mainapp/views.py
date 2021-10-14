6356bb18236d32f7c66c0449199a8db0435da738
oleg-ilin <www75@inbox.ru>
2021-10-14 04:39:47 +0300

    add lesson01

---
+++ mainapp/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'mainapp/index.html')
+
+
+def products(request):
+    return render(request, 'mainapp/products.html')
+
+
+def contact(request):
+    return render(request, 'mainapp/contact.html')
+
+# def index(request):
+#     return render(request, 'mainapp/index.html')
+#
+#
+# def contact(request):
+#     return render(request, 'mainapp/contact.html')
+#
+#
+# def products(request):
+#     return render(request, 'mainapp/products.html')
