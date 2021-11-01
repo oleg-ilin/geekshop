9d305948d13f324f024d262401679ddf04d42573
oleg <a@a.net>
2021-11-01 18:16:57 +0300

    ДЗ 5

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from mainapp.models import Product, ProductCategory
+from django.shortcuts import get_object_or_404
 
 
 def index(request):
@@ -18,8 +19,41 @@ def contact(request):
 
 
 def products(request, pk=None):
-    context = {
-        'links_menu': ProductCategory.objects.all(),
-        'title': 'Продукты'
+    print(pk)
+
+    title = 'продукты'
+    links_menu = ProductCategory.objects.all()
+
+    if pk is not None:
+        if pk == 0:
+            products = Product.objects.all().order_by('price')
+            category = {'name': 'все'}
+        else:
+            category = get_object_or_404(ProductCategory, pk=pk)
+            products = Product.objects.filter(category__pk=pk).order_by('price')
+
+        content = {
+            'title': title,
+            'links_menu': links_menu,
+            'category': category,
+            'products': products,
+        }
+
+        return render(request, 'mainapp/products_list.html', content)
+
+    same_products = Product.objects.all()[3:5]
+
+    content = {
+        'title': title,
+        'links_menu': links_menu,
+        'same_products': same_products
     }
-    return render(request, 'mainapp/products.html', context=context)
+
+    return render(request, 'mainapp/products.html', content)
+
+
+    # context = {
+    #     'links_menu': ProductCategory.objects.all(),
+    #     'title': 'Продукты'
+    # }
+    # return render(request, 'mainapp/products.html', context=context)
